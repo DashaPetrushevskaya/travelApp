b1aa813206efc7f219d5ad5beb92dbfa5c73b4f4
daryapetrushevskaya <daryapetrushevskaya@gmail.com>
2020-01-12 17:25:22 +0300

    no message

--- js/main.js
+++ js/main.js
@@ -1,19 +1,45 @@
 $('.slider-container').slick();
 
-/*(function() {
+$(".tourselectiontransport")
+.find("div")
+.click(function () {
+  let activeColor = "rgb(0, 164, 255)";
+  let noColor = "white";
+  let nextAfterActive = "#e4e4e4";
+  //remove current state
+  $(".tourselectiontransport").find("div").css("background-color", noColor);
 
-    var hamburger = {
-      navToggle: document.querySelector('.nav-toggle'),
-      nav: document.querySelector('nav'),
-  
-      doToggle: function(e) {
-        e.preventDefault();
-        this.navToggle.classList.toggle('expanded');
-        this.nav.classList.toggle('expanded');
+  //select active
+  $(this).css("background-color", activeColor);
+
+  //update next
+  let curActiveIndex = 0;
+  let tabsArray = [];
+  $(".tourselectiontransport").find("div")
+  .each(
+    function(index){
+      tabsArray.push($(this));
+
+      if($(this).css('background-color') == activeColor){
+        curActiveIndex = index;
       }
-    };
+    }
+  );
   
-    hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
-    hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
-  
-  }());*/
+  let nextTabIndex = 0;
+  if(curActiveIndex !== tabsArray.length - 1){
+    nextTabIndex = curActiveIndex + 1;
+  }
+  tabsArray[nextTabIndex].css("background-color", noColor);
+});
+
+$(".burger")
+.click(function () {
+  if($("ul.mob-container").hasClass("hidden")){
+    $("ul.mob-container").removeClass("hidden");
+  } 
+  else
+  {
+    $("ul.mob-container").addClass("hidden");
+  }
+});
