e60722769aa50e2a24bc4567e80519c3674c2537
daryapetrushevskaya <daryapetrushevskaya@gmail.com>
2020-01-26 16:46:34 +0300

    no message

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ $('.slider-container').slick({
   autoplaySpeed: 3000,
 });
 
+$('.sl-container').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+
 $(".burger")
 .click(function () {
   if($("ul.mob-container").hasClass("hidden")){
@@ -25,44 +30,9 @@ for (let anchor of anchors) {
     
     document.getElementById(blockID).scrollIntoView({
       behavior: 'smooth',
-      block: 'start'
+      block: 'start',
     })
   })
 }
-
-
-/*
-$(".tourselectiontransport")
-.find("div")
-.click(function () {
-  let activeColor = "rgb(0, 164, 255)";
-  let noColor = "white";
-  let nextAfterActive = "#e4e4e4";
-  //remove current state
-  $(".tourselectiontransport").find("div").css("background-color", noColor);
-
-  //select active
-  $(this).css("background-color", activeColor);
-
-  //update next
-  let curActiveIndex = 0;
-  let tabsArray = [];
-  $(".tourselectiontransport").find("div")
-  .each(
-    function(index){
-      tabsArray.push($(this));
-
-      if($(this).css('background-color') == activeColor){
-        curActiveIndex = index;
-      }
-    }
-  );
-  
-  let nextTabIndex = 0;
-  if(curActiveIndex !== tabsArray.length - 1){
-    nextTabIndex = curActiveIndex + 1;
-  }
-  tabsArray[nextTabIndex].css("background-color", noColor);
-});*/
 
 
